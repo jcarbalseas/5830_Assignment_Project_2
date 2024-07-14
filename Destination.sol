27a56495efe48cbaa9a6bc6466c6772adc911d51
jcarbalseas <49366476+jcarbalseas@users.noreply.github.com>
2024-07-14 11:10:47 -0700

    Update Destination.sol

--- Destination.sol
+++ Destination.sol
@@ -8,92 +8,55 @@ import "./BridgeToken.sol";
 contract Destination is AccessControl {
     bytes32 public constant WARDEN_ROLE = keccak256("BRIDGE_WARDEN_ROLE");
     bytes32 public constant CREATOR_ROLE = keccak256("CREATOR_ROLE");
-	mapping( address => address) public underlying_tokens;
-	mapping( address => address) public wrapped_tokens;
-	address[] public tokens;
+    mapping(address => address) public underlying_tokens;
+    mapping(address => address) public wrapped_tokens;
+    address[] public tokens;
 
-	event Creation( address indexed underlying_token, address indexed wrapped_token );
-	event Wrap( address indexed underlying_token, address indexed wrapped_token, address indexed to, uint256 amount );
-	event Unwrap( address indexed underlying_token, address indexed wrapped_token, address frm, address indexed to, uint256 amount );
+    event Creation(address indexed underlying_token, address indexed wrapped_token);
+    event Wrap(address indexed underlying_token, address indexed wrapped_token, address indexed to, uint256 amount);
+    event Unwrap(address indexed underlying_token, address indexed wrapped_token, address frm, address indexed to, uint256 amount);
 
-    constructor( address admin ) {
+    constructor(address admin) {
         _grantRole(DEFAULT_ADMIN_ROLE, admin);
         _grantRole(CREATOR_ROLE, admin);
         _grantRole(WARDEN_ROLE, admin);
     }
 
-	function wrap(address _underlying_token, address _recipient, uint256 _amount ) public onlyRole(WARDEN_ROLE) {
-		//YOUR CODE HERE
+    function wrap(address _underlying_token, address _recipient, uint256 _amount) public onlyRole(WARDEN_ROLE) {
+        //YOUR CODE HERE
         require(_amount > 0, "Amount must be greater than zero");
         require(wrapped_tokens[_underlying_token] != address(0), "Wrapped token not created");
 
-        // require(underlying_tokens[_underlying_token] != address(0), "Underlying asset not registered");
+        BridgeToken wrappedToken = BridgeToken(wrapped_tokens[_underlying_token]);
+        wrappedToken.mint(_recipient, _amount);
 
-        // address wrappedTokenAddress = underlying_tokens[_underlying_token];
-        // BridgeToken wrappedToken = BridgeToken(wrappedTokenAddress);
-
-        // ERC20 underlyingToken = ERC20(_underlying_token);
-        // require(underlyingToken.transferFrom(msg.sender, address(this), _amount), "Token transfer failed");
-
-        // wrappedToken.mint(_recipient, _amount);
-
-        // emit Wrap(_underlying_token, wrappedTokenAddress, _recipient, _amount);
         emit Wrap(_underlying_token, wrapped_tokens[_underlying_token], _recipient, _amount);
-	}
+    }
 
-	function unwrap(address _wrapped_token, address _recipient, uint256 _amount ) public {
-		//YOUR CODE HERE
+    function unwrap(address _wrapped_token, address _recipient, uint256 _amount) public {
+        //YOUR CODE HERE
+        require(_amount > 0, "Amount must be greater than zero");
         require(underlying_tokens[_wrapped_token] != address(0), "Underlying token not registered");
-        // require(wrapped_tokens[_underlying_token] != address(0), "Wrapped token not created");
-        // require(_amount > 0, "Amount must be greater than zero");
-        // require(wrapped_tokens[_wrapped_token] != address(0), "Wrapped token not registered");
 
-        // address underlyingTokenAddress = wrapped_tokens[_wrapped_token];
-        // BridgeToken wrappedToken = BridgeToken(_wrapped_token);
+        BridgeToken wrappedToken = BridgeToken(_wrapped_token);
+        wrappedToken.burnFrom(msg.sender, _amount);
 
-        // ERC20 underlyingToken = ERC20(underlyingTokenAddress);
-        // require(underlyingToken.transfer(_recipient, _amount), "Token transfer failed");
+        emit Unwrap(underlying_tokens[_wrapped_token], _wrapped_token, msg.sender, _recipient, _amount);
+    }
 
-        // wrappedToken.burnFrom(msg.sender, _amount);
-     
-        // wrapped_tokens[_wrapped_token] = address(0);
-        // underlying_tokens[underlyingTokenAddress] = address(0);
-        // tokens.remove(underlyingTokenAddress);
+    function createToken(address _underlying_token, string memory name, string memory symbol) public onlyRole(CREATOR_ROLE) returns (address) {
+        //YOUR CODE HERE
+        require(wrapped_tokens[_underlying_token] == address(0), "Token already created");
 
-        // emit Unwrap(underlyingTokenAddress, _wrapped_token, msg.sender, _recipient, _amount);
-	      emit Unwrap(underlying_tokens[_wrapped_token], _wrapped_token, msg.sender, _recipient, _amount);
-  }
+        BridgeToken newToken = new BridgeToken(_underlying_token, name, symbol, address(this)); // Assign address(this) as the minter
 
-	function createToken(address _underlying_token, string memory name, string memory symbol ) public onlyRole(CREATOR_ROLE) returns(address) {
-		//YOUR CODE HERE
-    require(wrapped_tokens[_underlying_token] == address(0), "Token already created");
+        underlying_tokens[_underlying_token] = address(newToken);
+        wrapped_tokens[address(newToken)] = _underlying_token;
 
-    BridgeToken newToken = new BridgeToken(_underlying_token, name, symbol, address(this)); // Assign address(this) as the minter
+        tokens.push(_underlying_token);
 
-    underlying_tokens[_underlying_token] = address(newToken);
-    wrapped_tokens[address(newToken)] = _underlying_token;
+        emit Creation(_underlying_token, address(newToken));
 
-    // No need to push to tokens array if it's not used elsewhere
-
-    emit Creation(_underlying_token, address(newToken));
-
-    // return address(newToken);
-        // if (underlying_tokens[_underlying_token] == address(0)){
-        //   return address(underlying_tokens[_underlying_token]);
-        // }
-        // require(underlying_tokens[_underlying_token] == address(0), "Token already created");
-
-        // BridgeToken newToken = new BridgeToken(_underlying_token, name, symbol, msg.sender);
-        
-        // underlying_tokens[_underlying_token] = address(newToken);
-        // wrapped_tokens[address(newToken)] = _underlying_token;
-        
-        // tokens.push(_underlying_token);
-
-        // emit Creation(_underlying_token, address(newToken));
-        
-        // return address(newToken);
-    
-	}
-
+        return address(newToken);
+    }
 }
